15c0d33125c3921585ff7f91f7331d1ee859a977
sviridov_av2 <sviridov_av2@magnit.ru>
2025-07-22 21:47:19 +0300

    update

--- tests/test_one.py
+++ tests/test_one.py
@@ -2,4 +2,4 @@ from selene import browser, be, have
 def test_no_match():
     browser.element('[name="q"]').should(be.visible).type("ыдлвофошывшфоытвофытвшофрывофытовлфтвыь").press_enter()
     (browser.element('[class="gSXOPxXJQAIpv4HDaDFd"]').should
-     (have.text('По запросу «adsdasвышгрфыш!№;%га561253!» ничего не найдено.')))
+     (have.text('По запросу «ыдлвофошывшфоытвофытвшофрывофытовлфтвыь» ничего не найдено.')))
