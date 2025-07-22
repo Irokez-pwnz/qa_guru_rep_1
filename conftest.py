5feef7b054c099d8963b95e26ea7cecca2e8ef9d
sviridov_av2 <sviridov_av2@magnit.ru>
2025-07-22 21:46:51 +0300

    update

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from selene import browser
 
-@pytest.fixture(scope = 'function' , autouse=True) # Фикстура открытия браузера(1200Х800) и открытие поисковика
+@pytest.fixture(scope = 'function' , autouse=True)
 def browser_open():
-    browser.config.window_width = 1200
-    browser.config.window_height = 800
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.open('https://duckduckgo.com')
     yield
     browser.quit()
--- tests/test_one.py
+++ tests/test_one.py
@@ -1,5 +1,5 @@
 from selene import browser, be, have
 def test_no_match():
-    browser.element('[name="q"]').should(be.visible).type("adsdasвышгрфыш!№;%га561253!").press_enter()
+    browser.element('[name="q"]').should(be.visible).type("ыдлвофошывшфоытвофытвшофрывофытовлфтвыь").press_enter()
     (browser.element('[class="gSXOPxXJQAIpv4HDaDFd"]').should
      (have.text('По запросу «adsdasвышгрфыш!№;%га561253!» ничего не найдено.')))
