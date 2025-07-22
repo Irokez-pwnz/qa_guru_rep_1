24563cd6a850ac4b9c522d5c49213d27ec6e02aa
sviridov_av2 <sviridov_av2@magnit.ru>
2025-07-22 21:42:35 +0300

    update

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,23 +1,5 @@
-import pytest
 from selene import browser, be, have
-
-
-@pytest.fixture(autouse=True)
-def browser_size():
- browser.config.window_height = 1366
- browser.config.window_width = 1024
- browser.open('https://duckduckgo.com')
- yield
- browser.quit()
-
-
-def test_selene_find_text():
- browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
- browser.element('html').should(have.text('Selene'))
-
-
-
-
-def test_selene_impossible_text():
- browser.element('[name="q"]').should(be.blank).type('sadhjlasfhljsdaf').press_enter()
- browser.element('html').should(have.text('По запросу «sadhjlasfhljsdaf» ничего не найдено.'))
+def test_no_match():
+    browser.element('[name="q"]').should(be.visible).type("adsdasвышгрфыш!№;%га561253!").press_enter()
+    (browser.element('[class="gSXOPxXJQAIpv4HDaDFd"]').should
+     (have.text('По запросу «adsdasвышгрфыш!№;%га561253!» ничего не найдено.')))
